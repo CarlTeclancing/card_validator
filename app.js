ca6397f115cb7b333fa95796eaab03609c757ae3
YuvCarl <carldrake969@gmail.com>
2022-08-18 13:35:45 +0100

    javascript code updated

---
+++ app.js
@@ -0,0 +1,18 @@
+let card = document.getElementById('card').value
+let Name = document.getElementById('name').value
+let date = document.getElementById('date').value
+let cv = document.getElementById('cv').value
+let submit = document.getElementById('submit').value
+
+ checkInput = () => {
+    if( (card == '') && (Name == '') && (date == '') && (cv == '')){
+       let errorMessage = document.getElementsByName('small')
+       errorMessage = ''
+       errorMessage.InnerText = 'error fill all elements'
+       errorMessage = errorMessage
+       console.log(errorMessage)
+    }
+    else{
+        console.log('good check out')
+    }
+}
